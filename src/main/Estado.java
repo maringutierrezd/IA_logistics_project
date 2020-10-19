8221fe078cdb16489c67415e59670cdc8c05b49e
marco.patino <marco.patino@estudiantat.upc.edu>
2020-10-19 12:06:21 +0200

    2º generador inicial

--- src/main/Estado.java
+++ src/main/Estado.java
@@ -110,11 +110,6 @@ public class Estado {
     //TODO: Asegurarse de que no se asignan a ofertas de prioridad mayor
     private int auxGenerador1 (int currentOfert, ArrayList <Integer> paq) {
         //Vamos asignando a cada paquete una oferta sin tener en cuenta el precio (pero calculandolo)
-        int noches_almacen;
-        int dias_viaje = T.get(currentOfert).getDias();
-        if (dias_viaje == 1) noches_almacen = 0;
-        else if (dias_viaje <= 3) noches_almacen = 1;
-        else noches_almacen = 2;
         //Asignamos los de prioridad1
         for (int i=0; i<paq.size(); ++i) {
             int paqueteActual = paq.get(i);
@@ -128,7 +123,7 @@ public class Estado {
             //si ya hemos llegado al final de las ofertas (poco probable)
             else if (currentOfert >= T.size()) {
                 //miramos si cabe en alguna de las ofertas anteriores
-                for (int j=0; j<T.size(); ++i) {
+                for (int j=0; j<T.size(); ++j) {
                     if (espLibre.get(j)>=P.get(paqueteActual).getPeso()) {
                         //asignamos paquete a oferta
                         asig.set(paqueteActual, j);
@@ -165,14 +160,67 @@ public class Estado {
 
         currentOfert = auxGenerador1 (currentOfert, paq1);
         currentOfert = auxGenerador1 (currentOfert, paq2);
-        currentOfert = auxGenerador1 (currentOfert, paq3);
+        auxGenerador1(currentOfert, paq3);
 
         System.out.println("Las asignaciones son: ");
         System.out.println(asig);
         calculaCoste(); calculaFelicidad();
     }
 
-    public void generador2() {
 
+    private int auxGenerador2 (int currentOfert, ArrayList<Integer> paq){
+        for (int i=0; i<paq.size(); ++i) {
+            int paqueteActual = paq.get(i);
+            if (currentOfert >=0 && espLibre.get(currentOfert)>=P.get(paqueteActual).getPeso()) {
+                //asignamos paquete a oferta
+                asig.set(paqueteActual, currentOfert);
+                // restamos el peso
+                espLibre.set(currentOfert, espLibre.get(currentOfert)-P.get(paqueteActual).getPeso());
+            }
+
+            //si ya hemos llegado al final de las ofertas (poco probable)
+            else if (currentOfert == 0) {
+                //miramos si cabe en alguna de las ofertas anteriores
+                for (int j=T.size()-1; j>=0; --j) {
+                    if (espLibre.get(j)>=P.get(paqueteActual).getPeso()) {
+                        //asignamos paquete a oferta
+                        asig.set(paqueteActual, j);
+                        // restamos el peso
+                        espLibre.set(j, espLibre.get(j)-P.get(paqueteActual).getPeso());
+                    }
+                    else System.out.println("Error! Paquetes no caben en ofertas");
+                }
+            }
+            //si el paquete no cabe en esta oferta
+            else {
+                --i;
+                --currentOfert;
+            }
+        }
+        return currentOfert;
+    }
+
+    public void generador2() {
+        //dividimos paquetes en 3 vectores segun prioridad. Esto tiene coste O(n), ordenarlos sería O(n*logn)
+        ArrayList <Integer> paq1 = new ArrayList <Integer> ();
+        ArrayList <Integer> paq2 = new ArrayList <Integer> ();
+        ArrayList <Integer> paq3 = new ArrayList <Integer> ();
+        int currentOfert = T.size()-1;
+
+        //Dividimos paquetes
+        for (int i=0; i<P.size(); ++i) {
+            int x = P.get(i).getPrioridad();
+            if (x == 0) paq1.add(i);
+            else if (x == 1) paq2.add(i);
+            else paq3.add(i);
+        }
+
+        currentOfert = auxGenerador2 (currentOfert, paq3);
+        currentOfert = auxGenerador2 (currentOfert, paq2);
+        auxGenerador2(currentOfert, paq1);
+
+        System.out.println("Las asignaciones son: ");
+        System.out.println(asig);
+        calculaCoste(); calculaFelicidad();
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,8 @@ import heuristicFunctions.*;
 import succesorFunctions.*;
 import aima.search.framework.Problem;
 import aima.search.framework.Successor;
+
+import java.util.Date;
 import java.util.Properties;
 
 import static java.lang.System.exit;
@@ -14,7 +16,7 @@ public class Main {
 
     public static void main (String[] args) {
         //Aqui hemos de definir los paquetes, las ofertas.. En definitiva, el problema
-        Paquetes paquetes = new Paquetes(9, 1234); //la seed 1234 es la del experimento que debemos hacer
+        Paquetes paquetes = new Paquetes(100, 1234); //la seed 1234 es la del experimento que debemos hacer
         Transporte transporte = new Transporte(paquetes, 1.2, 1234);
         //Ahora creamos el estado inicial
         Estado e = new Estado(paquetes, transporte);
@@ -22,15 +24,17 @@ public class Main {
         System.out.println("Los paquetes son " + paquetes.toString());
         System.out.println("Los ofertas son " + transporte.toString());
 
+
+        double time = new Date().getTime();
         //Generamos el estado (solucion inicial)
-        e.generador1();
+        e.generador2();
 
         System.out.println("La felicidad de la solución inicial es " + e.getFelicidad());
         System.out.println("El coste de la solución inicial es " + e.getCostes() + "\n");
 
         //Generamos el problema (solucion inicial, succesor function, goal test, heuristic function)
-        SuccesorF succ = new SuccesorF(true);
-        Problem problemHC = new Problem(e, succ, state->true, new Heuristica2());
+        SuccesorF succ = new SuccesorF(false);
+        Problem problemHC = new Problem(e, succ, state->true, new Heuristica1());
         try{
             HillClimbingSearch HCS = new HillClimbingSearch();
             SearchAgent agent = new SearchAgent(problemHC, HCS);
@@ -42,6 +46,7 @@ public class Main {
             System.out.println("FELICIDAD DE LA SOLUCIÓN: " + eFinal.getFelicidad());
             System.out.println("COSTE DE LA SOLUCIÓN: " + eFinal.getCostes());
             System.out.println("ESTADO FINAL:" + "\n" + eFinal.getAsig());
+            System.out.println("Ha tardado " + (new Date().getTime()-time)/1000.0 + " segundos. ");
 
 
         } catch (Exception exception) {
