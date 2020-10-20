4cb5bf920dfce90ec2f2d3f0f3c381d590c47b32
marco.patino <marco.patino@estudiantat.upc.edu>
2020-10-20 20:05:24 +0200

    .

--- src/heuristicFunctions/Heuristica1.java
+++ src/heuristicFunctions/Heuristica1.java
@@ -6,6 +6,7 @@ import main.Estado;
 public class Heuristica1 implements HeuristicFunction {
     public double getHeuristicValue(Object state) {
           Estado e = (Estado) state;
+        System.out.println(e.getCostes());
           return e.getCostes();
     }
 }
