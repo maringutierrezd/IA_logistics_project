3e8be9a3f436e4e507741a503d77b078fa5c2f27
marco.patino <marco.patino@estudiantat.upc.edu>
2020-10-20 15:38:34 +0200

    .

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
     public static void main (String[] args) {
         //Aqui hemos de definir los paquetes, las ofertas.. En definitiva, el problema
-        Paquetes paquetes = new Paquetes(1000, 1234); //la seed 1234 es la del experimento que debemos hacer
+        Paquetes paquetes = new Paquetes(10, 1234); //la seed 1234 es la del experimento que debemos hacer
         Transporte transporte = new Transporte(paquetes, 1.2, 1234);
         //Ahora creamos el estado inicial
         Estado e = new Estado(paquetes, transporte);
@@ -28,7 +28,9 @@ public class Main {
         double time = new Date().getTime();
         //Generamos el estado (solucion inicial)
         e.generador1();
-
+        System.out.println(paquetes);
+        System.out.println(transporte);
+        System.out.println(e.getAsig());
         System.out.println("La felicidad de la solución inicial es " + e.getFelicidad());
         System.out.println("El coste de la solución inicial es " + e.getCostes() + "\n");
 
--- src/succesorFunctions/Sucesor0.java
+++ src/succesorFunctions/Sucesor0.java
@@ -27,41 +27,158 @@ public class Sucesor0 implements SuccessorFunction {
         ArrayList<Double> actualEspLibre = actual.getEspLibre();
         ArrayList<Integer> actualAsig = actual.getAsig();
 
+        int actualFelicidad = actual.getFelicidad();
+        double actualCostes = actual.getCostes();
+
         for(int i=0; i<P.size(); ++i) {
             int ofetaActual = actualAsig.get(i);
             for(int j=0; j<T.size(); ++j) {
                 if(j!=ofetaActual && actualEspLibre.get(j) >= P.get(i).getPeso() && cumplePrio(P.get(i).getPrioridad(), T.get(j).getDias())) {
                     ArrayList<Double> newEspLibre = new ArrayList<Double>((ArrayList<Double>)actualEspLibre.clone());
                     ArrayList<Integer> newAsig = new ArrayList<Integer>((ArrayList<Integer>)actualAsig.clone());
-                    Estado sucesor = new Estado(newAsig, newEspLibre);
+
+                    int newFelicidad = actualFelicidad;
+                    double newCostes = actualCostes;
+                    Estado sucesor = new Estado(newFelicidad,newCostes, newEspLibre,newAsig);
 
                     set(sucesor, i, j); // aplicamos el operador set
 
                     // añadimos el sucesor a la lista
+                    System.out.println("Metemos el paquete " + i + " en la oferta" + j);
                     ret.add(new Successor("Metemos el paquete " + i + " en la oferta" + j, sucesor));
                 }
             }
         }
+        System.out.println("pasamos de iteracion");
+        if (meterIntercambiar){
+            for (int i=0; i<P.size(); ++i){
+                for (int j=i+1; j<P.size(); ++j){
+                    //Condiciones de aplicabilidad: que ambos quepan y que se cumplan prioridades
+                    int ofertaI = actualAsig.get(i);
+                    int ofertaJ = actualAsig.get(j);
+                    boolean cabeI = (actualEspLibre.get(ofertaJ)+P.get(j).getPeso()>=P.get(i).getPeso());
+                    boolean cabeJ = (actualEspLibre.get(ofertaI)+P.get(i).getPeso()>=P.get(j).getPeso());
+                    boolean cumplePrioI = cumplePrio(P.get(i).getPrioridad(),T.get(ofertaJ).getDias());
+                    boolean cumplePrioJ = cumplePrio(P.get(j).getPrioridad(),T.get(ofertaI).getDias());
+                    if (cabeI && cabeJ && cumplePrioI && cumplePrioJ){
+                        int newFelicidad = actualFelicidad;
+                        double newCostes = actualCostes;
+                        ArrayList<Double> newEspLibre = new ArrayList<Double>((ArrayList<Double>)actualEspLibre.clone());
+                        ArrayList<Integer> newAsig = new ArrayList<Integer>((ArrayList<Integer>)actualAsig.clone());
+                        //Creamos el nuevo estado con estos datos
+                        Estado sucesor = new Estado(newFelicidad, newCostes, newEspLibre, newAsig);
+                        //Cambiamos los datos del sucesor
+                        intecambiamosIconJ(sucesor, i, j);
+                        ret.add(new Successor("intercambiamos " + i + " con " + j, sucesor));
+                        System.out.println("intercambiamos" + i + " con " + j);
+                    }
+                }
+            }
+            System.out.println("pasamos de iteracion");
+        }
         return ret;
     }
+
+    private void intecambiamosIconJ(Estado e, int i, int j) {
+        //Deshacemos felicidad, coste y ocupacion
+        Paquetes p = e.getPaquetes();
+        Transporte t = e.getTransporte();
+
+        //Deshacemos felicidad
+        int felicidad = e.getFelicidad() - calcular_felicidad(e,i);
+        felicidad-= calcular_felicidad(e,j);
+
+        //Deshacemos costes
+        double costes = e.getCostes() - calcular_costes(e, i);
+        costes-=calcular_costes(e, j);
+
+        //Deshacemos espacio libre
+        ArrayList<Double> espLibre = (ArrayList<Double>)e.getEspLibre().clone();
+        int ofertaAnteriorI = e.getAsig().get(i);
+        int ofertaAnteriorJ = e.getAsig().get(j);
+        espLibre.set(ofertaAnteriorI, espLibre.get(ofertaAnteriorI)+p.get(i).getPeso());
+        espLibre.set(ofertaAnteriorJ, espLibre.get(ofertaAnteriorJ)+p.get(j).getPeso());
+
+        //Hacemos la nueva asignación
+        e.setAsig(i,ofertaAnteriorJ);
+        e.setAsig(j,ofertaAnteriorI);
+
+        //Hacemos felicidad
+        felicidad += calcular_felicidad(e, i) + calcular_felicidad(e,j);
+        //Hacemos costes
+        costes += calcular_costes(e,i) + calcular_costes(e,j);
+        //Hacemos espacio libre
+        espLibre.set(ofertaAnteriorJ,espLibre.get(ofertaAnteriorJ)+p.get(i).getPeso());
+        espLibre.set(ofertaAnteriorI,espLibre.get(ofertaAnteriorI)+p.get(j).getPeso());
+
+        //Seteamos felicidad costes y espacio libre
+        e.setFelicidad(felicidad);
+        e.setCostes(costes);
+        e.setEspLibre(espLibre);
+    }
+
+
+    // pre: el Estado e es un estado inicializado. El paquete i esta asignado a alguna oferta en el estado e
+    // post: devuelve el entero correspondiente a la felicidad que aporta i en el estado e
+    private int calcular_felicidad (Estado e, int i){
+        int ofertaAnterior = e.getAsig().get(i);
+        int prioridad = P.get(i).getPrioridad();
+        int dias = T.get(ofertaAnterior).getDias();
+        if (prioridad != 0) {
+            if (prioridad == 1) {if (dias==1) return 1; }
+            else {
+                if (dias< 4) return (4 - dias);
+            }
+        }
+        return 0;
+    }
+
+
+    // pre: el Estado e es un estado inicializado. El paquete i esta asignado a alguna oferta en el estado e
+    // post: devuelve el double correspondiente a los costes que supone i en el estado e.
+    private double calcular_costes (Estado e, int i){
+        int ofertaAnterior = e.getAsig().get(i);
+        int noches_almacen;
+        int dias_viaje = T.get(ofertaAnterior).getDias();
+        if (dias_viaje == 1) noches_almacen = 0;
+        else if (dias_viaje <= 3) noches_almacen = 1;
+        else noches_almacen = 2;
+        return P.get(i).getPeso()*T.get(ofertaAnterior).getPrecio() + 0.25 * P.get(i).getPeso() * noches_almacen;
+    }
+
+
 
     // pre: el Estado e es un estado inicializado. El Paquete identificado por i cumple todas las condiciones para ser
     //      asignado a la oferta j
     // post: el Paquete identificado por i pasa a ser asignado a la oferta j, actualizando los valores del Estado e para ser
     //       coherentes con su representación
     private void set(Estado e, int i, int j) {
-        ArrayList<Double> espLibre = e.getEspLibre();
+        Paquetes p = e.getPaquetes();
+        Transporte t = e.getTransporte();
+
+        //Deshacemos felicidad
+        int felicidad = e.getFelicidad() - calcular_felicidad(e,i);
+        //Deshacemos costes
+        double costes = e.getCostes() - calcular_costes(e, i);
+        //Deshacemos espacio libre
+        ArrayList<Double> espLibre = (ArrayList<Double>)e.getEspLibre().clone();
         int ofertaAnterior = e.getAsig().get(i);
+        espLibre.set(ofertaAnterior, espLibre.get(ofertaAnterior)+p.get(i).getPeso());
 
-        // liberamos el espacio ocupado por la oferta anterior del paquete i
-        espLibre.set(ofertaAnterior, espLibre.get(ofertaAnterior) + P.get(i).getPeso());
-        espLibre.set(j, espLibre.get(j) - P.get(i).getPeso());  // le quitamos espacio libre a la oferta j
+        //Hacemos la nueva asignación
+        e.setAsig(i,j);
 
-        e.setAsig(i, j);            // asignamos al paquete i la oferta j
-        e.setEspLibre(espLibre);    // actualizamos su espacio libre
+        //Hacemos felicidad
+        felicidad += calcular_felicidad(e, i);
+        //Hacemos costes
+        costes += calcular_costes(e,i);
+        //Hacemos espacio libre
+        espLibre.set(j,espLibre.get(j)-p.get(i).getPeso());
 
-        e.calculaCoste();           // llamamos a calculaCoste para actualizar los costes
-        e.calculaFelicidad();       // lo mismo con felicidad
+        //Seteamos felicidad costes y espacio libre
+        e.setFelicidad(felicidad);
+        e.setCostes(costes);
+        e.setEspLibre(espLibre);
     }
 
     // pre: cierto
